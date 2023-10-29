3ff6fe23d4c02c137260ab4d55cf40e209a513e1
Roman Gromov <truegrom@gmail.com>
2023-10-29 11:06:29 +0300

    handle profiles

--- src/main/java/com/parser/parser/ParserApplication.java
+++ src/main/java/com/parser/parser/ParserApplication.java
@@ -1,13 +1,30 @@
 package com.parser.parser;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ParserApplication {
 
+	@Autowired
+	Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ParserApplication.class, args);
+	}
+
+	@PostConstruct
+	private void logInitialization() {
+		System.out.println("                                                           ");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("ACTIVE PROFILE: " + Arrays.toString(env.getActiveProfiles()));
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("                                                           ");
 	}
 
 }
